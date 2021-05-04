f2b6f9775b22b0155d399805e5d7d4f5030e908d
rcc03461 <ych8815@gmail.com>
2021-05-04 20:36:17 +0800

    editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -61,6 +61,16 @@
             </div>
           </div>
         </li>
+        <li class="hover:bg-gray-300 relative">
+          <a 
+          @click.prevent="addImage"
+          href="#" 
+          class="h-12 px-3 flex justify-center items-center w-full focus:text-orange-500">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+            </svg>
+          </a>
+        </li>
       </ul>
       
       <!-- <a href="#" @click="clearSelect">C</a>
@@ -231,13 +241,18 @@ onMounted(()=>{
       isPositionOpened.value = !isPositionOpened.value;
     };
 
+    const addImage = () => {
+      // isPositionOpened.value = !isPositionOpened.value;
+    };
+
     return { 
       selectedUUiD,
       pageElements, 
       isPositionOpened, 
       setMe ,
       clearSelect,
-      togglePosition
+      togglePosition,
+      addImage
     }
 
   }
